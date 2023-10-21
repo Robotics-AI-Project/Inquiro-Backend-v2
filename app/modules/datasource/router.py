14213d03c7e6443ab4251ab14bda889be418952d
tontan2545 <tontan2545@gmail.com>
2023-10-21 17:53:52 +0700

    feat: C-R datasource api

--- app/modules/datasource/router.py
+++ app/modules/datasource/router.py
@@ -1,16 +1,51 @@
-from fastapi import APIRouter, Depends
-from starlette.requests import Request
+from typing import Annotated
 
-from app.dependencies import get_auth_header, AuthPayload
+from fastapi import APIRouter, Depends, HTTPException
+from prisma.errors import PrismaError
+
+from app.modules.datasource.dto import CreateDatasourceDTO
+from app.utils import prisma
+from prisma.models import User
+
+from app.dependencies.auth import get_user
 
 router = APIRouter(
-    prefix="/data-sources",
-    tags=["data-sources"],
-    dependencies=[Depends(get_auth_header)],
+    prefix="/datasource",
+    tags=["datasource"],
 )
 
 
 @router.get("/")
-async def root(request: Request):
-    payload: AuthPayload = request.state.payload
-    return {"message": payload["user_id"]}
+async def get_all_datasource(user: Annotated[User, Depends(get_user)]):
+    try:
+        datasource = await prisma.datasource.find_many(where={"userId": user.id})
+        return datasource
+    except PrismaError:
+        return []
+
+
+@router.delete("/{datasource_id}")
+async def delete_datasource(datasource_id: int, _: Annotated[User, Depends(get_user)]):
+    try:
+        datasource = await prisma.datasource.delete(where={"id": datasource_id})
+        return datasource
+    except PrismaError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/")
+async def create_datasource(
+    body: CreateDatasourceDTO, user: Annotated[User, Depends(get_user)]
+):
+    try:
+        datasource = await prisma.datasource.create(
+            data={
+                "userId": user.id,
+                "name": body.name,
+                "engine": body.engine,
+                "url": body.url,
+            }
+        )
+        return datasource
+    except PrismaError as e:
+        raise HTTPException(status_code=400, detail=str(e))
