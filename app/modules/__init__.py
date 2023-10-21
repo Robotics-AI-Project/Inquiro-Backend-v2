025cde12d8c934e255af4e192e12bf8cf1adaf54
tontan2545 <tontan2545@gmail.com>
2023-10-21 23:21:29 +0700

    feat: renamed module

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter
 
 from .chat.router import router as chat_router
-from .sql.router import router as sql_router
+from .inference.router import router as inference_router
 from .datasource.router import router as datasource_router
 from .dashboard.router import router as dashboard_router
 from .snippet.router import router as snippet_router
 
 _routers = [
     chat_router,
-    sql_router,
+    inference_router,
     datasource_router,
     dashboard_router,
     snippet_router,
